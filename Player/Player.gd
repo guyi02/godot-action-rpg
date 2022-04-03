eae7515beed758daba6c17b84b4e229b2ad8fd76
guyi02 <guyi__@hotmail.com>
2022-04-03 10:38:09 -0300

    add move to character

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,9 @@ const MAX_SPEED = 80
 const FRICTION = 600
 
 var velocity = Vector2.ZERO 
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 # movimentação do personagem
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -13,8 +16,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	# colisão do personagem
